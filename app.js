56fe6021d92c19fb20b8d56d7021a217011a40c3
HammedBabatunde <55561306+HammedBabatunde@users.noreply.github.com>
2021-01-07 03:23:00 +0100

    app.js file

---
+++ app.js
@@ -0,0 +1,21 @@
+// Importing packages
+const request = require("request");
+const dotenv = require("dotenv").config()
+
+const address = process.argv[2]
+
+
+// Ensure you create your API KEY on a .env file
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${process.env.API_KEY}` 
+
+
+if (!address) {
+    return  console.log("Please enter the name of the city")
+}
+
+request(url, (error, response, body) => {
+    const data = JSON.parse(body)
+    console.log(`It's currently ${data.main.temp} outside`)
+})
+
+
